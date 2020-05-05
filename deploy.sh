b0340259f40761c92ec1c102027375c1a3a46f52
jordan396 <jordan.chyehong@gmail.com>
2020-05-05 14:33:15 +0800

    Modified deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
-docker build -t jordan396/multi-client -f ./client/Dockerfile ./client
-docker build -t jordan396/multi-server -f ./server/Dockerfile ./server
-docker build -t jordan396/multi-worker -f ./worker/Dockerfile ./worker
-docker push jordan396/multi-client
-docker push jordan396/multi-server
-docker push jordan396/multi-worker
-kubectl rollout restart -f k8s-config
+docker build -t jordan396/multi-client:latest -t jordan396/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jordan396/multi-server:latest -t jordan396/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jordan396/multi-worker:latest -t jordan396/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push jordan396/multi-client:latest
+docker push jordan396/multi-server:latest
+docker push jordan396/multi-worker:latest
+
+docker push jordan396/multi-client:$SHA
+docker push jordan396/multi-server:$SHA
+docker push jordan396/multi-worker:$SHA
+
+kubectl apply -f k8s-config
+kubectl set image deployments/server-deployment server=jordan396/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jordan396/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jordan396/multi-worker:$SHA
