2d18c529b2881f1ffe3c5bfcb9acdb8b1f80da25
jordan396 <jordan.chyehong@gmail.com>
2020-05-05 13:41:48 +0800

    Modified deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-docker build -t Jordan396/multi-client -f ./client/Dockerfile ./client
-docker build -t Jordan396/multi-server -f ./server/Dockerfile ./server
-docker build -t Jordan396/multi-worker -f ./worker/Dockerfile ./worker
-docker push Jordan396/multi-client
-docker push Jordan396/multi-server
-docker push Jordan396/multi-worker
+docker build -t jordan396/multi-client -f ./client/Dockerfile ./client
+docker build -t jordan396/multi-server -f ./server/Dockerfile ./server
+docker build -t jordan396/multi-worker -f ./worker/Dockerfile ./worker
+docker push jordan396/multi-client
+docker push jordan396/multi-server
+docker push jordan396/multi-worker
 kubectl rollout restart -f k8s-config
